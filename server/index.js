8aaab650da3e217806218382a3ce9a57ee757940
Christopher Ksiazak <cksiazak@gmail.com>
2020-02-05 23:13:51 -0700

    Added both backend and frontend, and initialized socket'

---
+++ server/index.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const socketio = require('socket.io');
+const http = require('http');
+
+// init server
+const app = express();
+const server = http.createServer(app);
+const io = socketio(server);
+
+// init websocket
+io.on('connection', socket => {
+  console.log('We have a new connection');
+
+  // socket message on disconnect
+  socket.on('disconnect', () => {
+    console.log('User has left');
+  });
+});
+
+// route
+app.use('/', require('./router'));
+
+// port for server
+const PORT = process.env.PORT || 5000;
+
+server.listen(PORT, () => {
+  console.log(`Sever running on port ${PORT}`);
+});
