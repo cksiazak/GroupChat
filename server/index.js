4069375b8611ec786904e3733a61e28d66cc470b
Christopher Ksiazak <cksiazak@gmail.com>
2020-02-06 22:45:45 -0700

    added joining, exiting and messaging events

--- server/index.js
+++ server/index.js
@@ -2,6 +2,8 @@ const express = require('express');
 const socketio = require('socket.io');
 const http = require('http');
 
+const { addUser, removeUser, getUser, getUsersInRoom } = require('./users.js');
+
 // init server
 const app = express();
 const server = http.createServer(app);
@@ -10,16 +12,56 @@ const io = socketio(server);
 // init websocket
 // check for connection ON websocket
 io.on('connection', socket => {
-  // init with console message
-  console.log('We have a new connection');
-
   // socket on join, emitted from client
   // client sends a 'join' param
+  // data is what we are receiving
+  // cb is what we can emit back once certain conditions are met
   socket.on('join', (data, cb) => {
-    // data is what we are receiving
-    console.log(data);
+    // addUser returns either an obj with a user, or an err
+    // passing in the instance of the socket as the id
+    // and spreading in the data object we receive
+    const { user, error } = addUser({ id: socket.id, ...data });
 
-    // cb is what we can emit back once certain conditions are met
+    // if there is an error,
+    // return the callback with the error
+    // this will emit to the front end
+    if (error) {
+      return cb(error);
+    }
+
+    // system message directly to someone when they join a room
+    // sending a payload with the user 'admin' and a message
+    socket.emit('message', {
+      user: 'admin',
+      text: `${user.name}, welcome to the room ${user.room}`
+    });
+
+    // sends message to everyone else, besides our user.
+    socket.broadcast
+      .to(user.room)
+      .emit('message', { user: 'admin', text: `${user.name} has joined` });
+
+    // if there isn't an error
+    // call socket method join to join a user in a room
+    socket.join(user.room);
+
+    // add this so the callback in the front end gets called every time
+    cb();
+  });
+
+  // expect an event from the front end
+  // with a sendMessage param
+  socket.on('sendMessage', (message, cb) => {
+    // we need to get the user that sent the message
+    // by passing in the socket.id (which is their uid)
+    const user = getUser(socket.id);
+
+    // target a room, by passing in our user's room
+    // and emitting a message to that room
+    io.to(user.room).emit('message', { user: user.name, text: message });
+
+    // call cb so we can do something afterwards
+    cb();
   });
 
   // socket message on disconnect
